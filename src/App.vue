02293cf0f2816d5580b0a36df9e48b9749629a55
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-02-07 10:41:21 +0100

    first real commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper underline bg-green-600">
+      <HelloWorld class="" msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
