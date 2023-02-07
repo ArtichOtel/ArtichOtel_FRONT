768ae9ab5bcbcc1c8adcc470781309c708cc0169
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-02-07 16:29:55 +0100

    homeView architecture

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
 
+import Header from "../components/sections/Header.vue";
+import Hero from "../components/sections/Hero.vue";
+import Footer from "../components/sections/Footer.vue";
+import Reviews from "../components/sections/Reviews.vue";
+import Video from "../components/sections/Video.vue";
+import Offers from "../components/sections/Offers.vue";
+import Advantages from "../components/sections/Advantages.vue";
+import RoomsTypes from "../components/sections/RoomsTypes.vue";
+import NewsInfo from "../components/sections/NewsInfo.vue";
 </script>
 
 <template>
   <main>
-    nothing here
+    <Header/>
+    <Hero/>
+    <Offers/>
+    <Advantages/>
+    <NewsInfo/>
+    <RoomsTypes/>
+    <Video/>
+    <Reviews/>
+    <Footer/>
   </main>
 </template>
