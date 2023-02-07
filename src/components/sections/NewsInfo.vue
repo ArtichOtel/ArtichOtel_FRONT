5d5f70c487edd55a293865b071d2146ae0f056d6
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-02-07 16:29:55 +0100

    homeView architecture

---
+++ src/components/sections/NewsInfo.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "NewsInfo"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,26 @@
-<script>
-export default {
-  name: "Home",
-  components: {},
-};
+<script setup>
+
+import Header from "../components/sections/Header.vue";
+import Hero from "../components/sections/Hero.vue";
+import Footer from "../components/sections/Footer.vue";
+import Reviews from "../components/sections/Reviews.vue";
+import Video from "../components/sections/Video.vue";
+import Offers from "../components/sections/Offers.vue";
+import Advantages from "../components/sections/Advantages.vue";
+import RoomsTypes from "../components/sections/RoomsTypes.vue";
+import NewsInfo from "../components/sections/NewsInfo.vue";
 </script>
 
 <template>
   <main>
-    <h1>Home</h1>
+    <Header/>
+    <Hero/>
+    <Offers/>
+    <Advantages/>
+    <NewsInfo/>
+    <RoomsTypes/>
+    <Video/>
+    <Reviews/>
+    <Footer/>
   </main>
 </template>
