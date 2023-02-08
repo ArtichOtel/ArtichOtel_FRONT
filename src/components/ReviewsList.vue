ae48349daff040229e2865db3b654ea0a2249a2d
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-02-08 14:17:35 +0100

    add get reviews for testing

---
+++ src/components/ReviewsList.vue
@@ -0,0 +1,18 @@
+<template>
+  Liste des avis :
+<ul>
+  <li v-for="review in allReviews" v-bind:key="review.id">{{review.title}}</li>
+</ul>
+</template>
+
+<script>
+export default {
+  name: "ReviewsList",
+  props: {
+    allReviews: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
