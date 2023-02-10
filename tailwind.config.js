a292cd5bbaa6742d5fa97dc43af347fb882fdead
dakotaFroissart <dakota.froissart@le-campus-numerique.fr>
2023-02-10 11:33:49 +0100

    Added bitter and nunito-sans to tailwind #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'fontFamily': {
+        'title': ['Bitter', 'serif'],
+        'content': ['Nunito Sans', 'sans-serif'],
+      } 
+    },
   },
   plugins: [],
 }
