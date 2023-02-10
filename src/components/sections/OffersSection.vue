6f9ba2dfe39fe8ef83e2bdc83f753c96ab5a6f07
dakotaFroissart <dakota.froissart@le-campus-numerique.fr>
2023-02-10 11:51:48 +0100

    #3 first structure

--- src/components/sections/OffersSection.vue
+++ src/components/sections/OffersSection.vue
@@ -1,11 +1,48 @@
 <template>
+  <section class="px-[calc(8.6vw)]">
 
+    <div class="flex flex-row justify-between gap-[calc(9.6vw)]">
+
+      <div class="grid grid-cols-1 gap-1">
+
+        <h2 class="font-title">{{ title }}</h2>
+        <span class="font-content">{{ offer.description }}</span>
+
+      </div>
+      <div>
+
+        <img :src="offer.url_image" alt="">
+
+      </div>
+
+    </div>
+
+  </section>
 </template>
 
-<script>
-export default {
-  name: "OffersSection"
-}
+<script setup>
+
+import axios from 'axios';
+import { ref } from 'vue';
+
+const offer = ref({})
+const props = defineProps({
+  title: String,
+  uri: String
+})
+
+axios({
+  method: 'get',
+  url: `${import.meta.env.VITE_API_BASE_URL}${props.uri}`
+})
+  .then((response) => {
+    console.log("offers", response.data)
+    offer.value = response.data[0]
+  })
+  .catch((error) => {
+    console.log('Error: ', error)
+  })
+
 </script>
 
 <style scoped>
