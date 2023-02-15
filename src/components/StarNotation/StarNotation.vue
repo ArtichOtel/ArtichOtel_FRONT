aaa876a85db89451ad993f10aa4ab35bb38e7a8b
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-02-15 09:41:35 +0100

    reviews ok #2

--- src/components/StarNotation/StarNotation.vue
+++ src/components/StarNotation/StarNotation.vue
@@ -1,9 +1,11 @@
 <template>
-  <component :is="nOfStar > 0 ? FilledStar : EmptyStar"/>
-  <component :is="nOfStar > 1 ? FilledStar : EmptyStar"/>
-  <component :is="nOfStar > 2 ? FilledStar : EmptyStar"/>
-  <component :is="nOfStar > 3 ? FilledStar : EmptyStar"/>
-  <component :is="nOfStar > 4 ? FilledStar : EmptyStar"/>
+  <div class="flex">
+    <component :is="nOfStar > 0 ? FilledStar : EmptyStar"/>
+    <component :is="nOfStar > 1 ? FilledStar : EmptyStar"/>
+    <component :is="nOfStar > 2 ? FilledStar : EmptyStar"/>
+    <component :is="nOfStar > 3 ? FilledStar : EmptyStar"/>
+    <component :is="nOfStar > 4 ? FilledStar : EmptyStar"/>
+  </div>
 </template>
 
 <script>
