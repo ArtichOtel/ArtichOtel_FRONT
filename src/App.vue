aa25d104e92b8752a8d6121b5499a491dec8a88d
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-02-17 15:27:01 +0100

    logout ok #9

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template class="font-content scroll-smooth">
-  <LoginButton />
-  <RouterView v-bind:sections="data" />
+  <LoginButton @logoutRequest="logout" v-bind:isLogged="isLogged" />
+  <RouterView v-bind:sections="data" v-on:login="setLogged" />
 </template>
 
 <script setup>
 import {ref} from "vue";
 import LoginButton from "./components/LoginButton.vue";
 import axios from "axios";
+import router from "./router";
 
 const connectionStatus = ref({
   connected: false,
@@ -14,6 +15,7 @@ const connectionStatus = ref({
 })
 
 const data = ref([])
+const isLogged = ref(false)
 
 axios.get(`${import.meta.env.VITE_API_BASE_URL}/sections`)
     .then((response) => {
@@ -22,6 +24,40 @@ axios.get(`${import.meta.env.VITE_API_BASE_URL}/sections`)
     .catch((err)=> {
       console.log("BUG :", err)
     })
+
+
+function logout() {
+  axios.get(`${import.meta.env.VITE_API_BASE_URL}/user/logout`, {
+    headers: {
+      'Authorization': `Bearer ${window.sessionStorage.getItem('token')}`
+    }
+  })
+      .then(response => {
+        if (response.status === 200) {
+          alert("Vous êtes correctement déconnecté")
+        }
+  }).catch(err => {
+    alert("Il y a eu une erreur lors de la déconnexion")
+    console.log(err)
+  })
+
+  window.sessionStorage.clear()
+  router.push("/")
+}
+function setLogged() {
+  console.log("app knows that you are logged")
+  isLogged.value = true
+}
+
+
+function checkIfLogged(data) {
+  if ( window.sessionStorage.getItem('token')) {
+    isLogged.value = true
+  } else {
+    isLogged.value = false
+  }
+}
+
 
 
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template >
   <main v-if="sections.length" class="h-full w-full overflow-hidden">
     <HeaderSection :title="sections[0].title" :uri="sections[0].uri" />
-    <LoginForm @connectionStatus="(data) => sayHello(data)"/>
+    <LoginForm @connectionStatus="(data) => {sayHello(data)}"/>
     <FooterSection :title="sections[7].title" :uri="sections[7].uri" />
   </main>
 </template>
@@ -19,18 +19,30 @@ import FooterSection from "../components/sections/FooterSection.vue";
 import LoginForm from "../components/forms/LoginForm.vue";
 import router from "../router";
 
-
 const props = defineProps({
   sections: Array
 })
+
+const emit = defineEmits(
+    {'login': {}}
+)
+
 function sayHello(data) {
   console.log("vous êtes connecté")
-  console.group(data)
+  //console.group(data)
 
-  if (data.role === "admin") {
+  if (data.token) {
+    emit('login')
+  }
+
+/*  if (data.role === "admin") {
     console.log("redirect to admin panel")
     router.push("/admin")
-  }
+  }*/
+
+  router.push("/")
 }
+
+
 
 </script>
