6451220d9c5103fc786ff63fbeec7d6f10e26070
dakotaFroissart <dakota.froissart@le-campus-numerique.fr>
2023-02-17 08:50:02 +0100

    #10 Fix by changing given path for svg

--- src/components/LoginButton.vue
+++ src/components/LoginButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-primary text-white absolute right-0 top-16 w-10 h-10 flex justify-center items-center z-100">
-    <RouterLink to="/login"><img src="src/assets/icons/user.svg" alt="" class="fill-current"></RouterLink>
+    <RouterLink to="/login"><img src="/src/assets/icons/user.svg" alt="" class="fill-current"></RouterLink>
 
   </div>
 </template>
