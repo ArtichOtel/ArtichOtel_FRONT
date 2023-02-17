3caa6a9a61a9815c4d9248ef1f2a8811f4e27393
dakotaFroissart <dakota.froissart@le-campus-numerique.fr>
2023-02-17 10:03:03 +0100

    #11 Fix

--- src/components/LoginButton.vue
+++ src/components/LoginButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-primary text-white absolute right-0 top-16 w-10 h-10 flex justify-center items-center z-100">
+  <div class="bg-primary text-white fixed right-0 top-16 w-10 h-10 flex justify-center items-center z-100">
     <RouterLink to="/login"><img src="/src/assets/icons/user.svg" alt="" class="fill-current"></RouterLink>
 
   </div>
