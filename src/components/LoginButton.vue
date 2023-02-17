010e2bafc110529e5fcc5992b66d3167bfd33e14
dakotaFroissart <dakota.froissart@le-campus-numerique.fr>
2023-02-17 11:59:26 +0100

    #14 Better SVG header

--- src/components/LoginButton.vue
+++ src/components/LoginButton.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="bg-primary text-white fixed right-0 top-16 w-10 h-10 flex justify-center items-center z-100">
-    <RouterLink to="/login"><img src="/src/assets/icons/user.svg" alt="" class="fill-current"></RouterLink>
+  <div class="bg-secondary text-white fixed right-0 top-16 w-10 h-10 flex
+            justify-center items-center z-100 rounded-l-lg hover:invert duration-500 hover:duration-200">
+    <RouterLink to="/login"><img src="/src/assets/icons/profile-circle.svg" alt=""></RouterLink>
 
   </div>
 </template>
--- src/components/sections/HeaderSection.vue
+++ src/components/sections/HeaderSection.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="fixed w-full z-50">
     <div
-      class="relative w-full flex flex-wrap items-center justify-between py-3 bg-primary text-gray-200 shadow-lg navbar navbar-expand-lg"
+      class="relative w-full flex flex-wrap items-center justify-between py-3 bg-primary text-secondary shadow-lg navbar navbar-expand-lg"
     >
       <div
         class="container-fluid w-full flex flex-wrap items-center justify-between px-6"
@@ -18,12 +18,16 @@
         <!-- Right elements -->
         <div class="flex items-center relative gap-10">
           <div class="flex gap-5">
-            <img src="#" alt="logo-email-artichotel" />
-            <button class="font-content">info@hotelartichaut.fr</button>
+            <a class="font-content" href="contact:info@hotelartichaut.fr">
+              <img src="/src/assets/icons/sms.svg" alt="logo-email-artichotel" class="float-left mr-3 svg-secondary"/>
+              <span> info@hotelartichaut.fr</span>
+            </a>
           </div>
           <div class="flex gap-5">
-            <img src="#" alt="logo-telephone-artichotel" />
-            <button class="font-content">(+33) 4 75 00 00 00</button>
+            <a class="font-content" href="tel:+33475000000">
+              <img src="/src/assets/icons/call.svg" alt="logo-telephone-artichotel" class="float-left mr-3 svg-secondary"/>
+              <span>(+33) 4 75 00 00 00</span>
+            </a>
           </div>
         </div>
         <!-- Right elements -->
