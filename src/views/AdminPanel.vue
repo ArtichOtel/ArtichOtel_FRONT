b84531cb6d1ac64cb3d46d530a953b9e32eb7d6b
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-02-17 13:03:06 +0100

    login success push to admin, admin is secured #7

--- src/views/AdminPanel.vue
+++ src/views/AdminPanel.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="bg-secondary text-tertiary h-screen">
+  <main v-if="isAdmin" class="bg-secondary text-tertiary h-screen">
     <h1>Admin Panel</h1>
     <SelectionSelector
         @clickOnBtn="(index) => setCurrentSection(index)" :list-of-sections="listOfSections"
@@ -21,6 +21,8 @@ export default {
 import SelectionSelector from "../components/admin/SelectionSelector.vue";
 import HeroEditor from "../components/admin/HeroEditor.vue";
 import {ref} from "vue";
+import axios from "axios";
+import router from "../router";
 
 const listOfSections = ref([
   {name:"Header", comp:null},
@@ -36,17 +38,33 @@ const listOfSections = ref([
 const currentIndex = ref(null)
 const currentSection = ref({})
 
+const isAdmin = ref(false)
+
 function setCurrentSection(index) {
   console.log(index)
   currentIndex.value = index
   currentSection.value = listOfSections.value[index]
 }
-/*
 
+// check if user is admin by its token : any method on api/user need admin token
+axios.get(`${import.meta.env.VITE_API_BASE_URL}/user`, {
+  headers: {
+    'Authorization': `Bearer ${window.sessionStorage.getItem('token')}`
+  }
+})
+    .then((response) => {
+      console.log(response.data)
 
-onMount() {
- check role
-}
-*/
+      if (response.status === 200) {
+        isAdmin.value = true
+      } else {
+        router.push("/")
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      router.push("/")
+    })
+
 
 </script>
