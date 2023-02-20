05aeea0db526f6c2c7ea9affa7231191dd51de83
Anthony26100 <anthony.bellier@laposte.net>
2023-02-20 09:29:09 +0100

    pagenotfound in progress

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,6 @@
+<template><h1>PageNotFound</h1></template>
+<script>
+export default {
+  name: "PageNotFound",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <main v-if="sections.length" class="h-full w-full overflow-hidden">
     <HeaderSection />
-    <HeroSection        :title="sections[0].title" :uri="sections[0].uri" />
-    <OffersSection      :title="sections[1].title" :uri="sections[1].uri" />
-    <AdvantagesSection  :title="sections[2].title" :uri="sections[2].uri" />
-    <NewsInfoSection    :title="sections[3].title" :uri="sections[3].uri" />
-    <RoomsTypesSection  :title="sections[5].title" :uri="sections[5].uri" />
-    <ReviewsSection     :title="sections[4].title" :uri="sections[4].uri"/>
-    <VideoSection       :title="sections[6].title" :uri="sections[6].uri" />
-    <FooterSection      :title="sections[7].title" :uri="sections[7].uri" />
+    <HeroSection :title="sections[0].title" :uri="sections[0].uri" />
+    <OffersSection :title="sections[1].title" :uri="sections[1].uri" />
+    <AdvantagesSection :title="sections[2].title" :uri="sections[2].uri" />
+    <NewsInfoSection :title="sections[3].title" :uri="sections[3].uri" />
+    <RoomsTypesSection :title="sections[5].title" :uri="sections[5].uri" />
+    <ReviewsSection :title="sections[4].title" :uri="sections[4].uri" />
+    <VideoSection :title="sections[6].title" :uri="sections[6].uri" />
+    <FooterSection :title="sections[7].title" :uri="sections[7].uri" />
+    <PageNotFound />
   </main>
 </template>
-
 
 <script setup>
 import HeaderSection from "../components/sections/HeaderSection.vue";
@@ -23,13 +23,9 @@ import OffersSection from "../components/sections/OffersSection.vue";
 import AdvantagesSection from "../components/sections/AdvantagesSection.vue";
 import RoomsTypesSection from "../components/sections/RoomsTypesSection.vue";
 import NewsInfoSection from "../components/sections/NewsInfoSection.vue";
-
+import PageNotFound from "../components/PageNotFound.vue";
 
 const props = defineProps({
-  sections: Array
-})
-
-
+  sections: Array,
+});
 </script>
-
-
