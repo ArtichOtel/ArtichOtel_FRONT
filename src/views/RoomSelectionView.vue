9b0e6628068672ef950ea97456f9a8c6b8a2aea9
Anthony26100 <anthony.bellier@laposte.net>
2023-03-03 13:27:24 +0100

    (chore) add component into RoomSelection view

--- src/views/RoomSelectionView.vue
+++ src/views/RoomSelectionView.vue
@@ -4,29 +4,31 @@
     <div
       class="relative h-full mt-12 bg-primary/40 py-8 px-8 md:py-[4vw] md:px-[8.6vw]"
     >
-      <NavBar />
-
       <div class="flex flex-col justify-center items-center bg-secondary">
         <h2
           class="font-title text-titleBase md:text-titleMed mt-[4vw] mb-[2vw]"
         >
-          Sélection de chambre
+          {{ dico[langStore.lang].roomSelectionTitle }}
         </h2>
 
         <form class="mb-[2vw] tracking-wider">
           <div class="flex flex-wrap justify-center gap-[2vw] mb-[3vw]">
             <!--    left picker        -->
             <DatePicker
-              v-bind:title="'Arrivée'"
+              v-bind:title="dico[langStore.lang].datePickerArrivalTitle"
               v-bind:boundary="'start'"
               v-bind:svgColor="'svg-tertiary'"
+              v-bind:maxDate="''"
+              v-bind:minDate="minDateStart"
             />
 
             <!--    right picker        -->
             <DatePicker
-              v-bind:title="'Départ'"
+              v-bind:title="dico[langStore.lang].datePickerDepartureTitle"
               v-bind:boundary="'end'"
               v-bind:svgColor="'svg-tertiary'"
+              v-bind:maxDate="''"
+              v-bind:minDate="minDateEnd"
             />
 
             <NOfNights
@@ -41,11 +43,12 @@
               v-on:click="signup"
               class="bg-accent flex justify-center items-center w-64 h-9 text-xl tracking-wider text-tertiary font-semibold font-content uppercase"
             >
-              rechercher
+              {{ dico[langStore.lang].roomSelectionCTA }}
             </button>
           </div>
         </form>
       </div>
+      <BookingFetch />
     </div>
   </main>
   <FooterSection
@@ -58,6 +61,7 @@
 <script>
 export default {
   name: "SignUpView",
+  components: { BookingFetch },
 };
 </script>
 
@@ -69,6 +73,10 @@ import NavBar from "../components/navigation/NavBar.vue";
 import DatePicker from "../components/blocks/DatePicker.vue";
 import NOfNights from "../components/blocks/NOfNights.vue";
 import { useQueryDatesStore } from "../stores/queryDates";
+import { computed, ref } from "vue";
+import { addDays, formatISO } from "date-fns";
+import { i18n } from "../utils/i18n";
+import { useLangStore } from "../stores/lang";
 import BookingFetch from "../components/bookingFetch/bookingFetch.vue";
 
 const props = defineProps({
@@ -76,4 +84,15 @@ const props = defineProps({
 });
 
 const queryDateStore = useQueryDatesStore();
+const langStore = useLangStore();
+const dico = i18n;
+
+const minDateStart = ref(
+  formatISO(addDays(new Date(), 0.5), { representation: "date" })
+);
+const minDateEnd = computed(() => {
+  return formatISO(addDays(queryDateStore["start"].date, 1.5), {
+    representation: "date",
+  });
+});
 </script>
