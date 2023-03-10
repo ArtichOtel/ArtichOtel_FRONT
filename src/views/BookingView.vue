85a90e90f3dddc64f3d01b9c69d877292ee1259e
Anthony <anthony.bellier@laposte.net>
2023-03-10 13:07:17 +0100

    (chore) add methode PUT in axios

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -1,203 +1,209 @@
-<template >
+<template>
   <HeaderSection />
   <main class="w-full overflow-hidden">
-    <div class="static h-full mt-12 bg-primary/40 py-8 px-8 md:py-[4vw] md:px-[8.6vw]">
-
+    <div
+      class="static h-full mt-12 bg-primary/40 py-8 px-8 md:py-[4vw] md:px-[8.6vw]"
+    >
       <div class="flex flex-col justify-center items-center bg-secondary">
-        <h2 class="font-title text-titleBase md:text-titleMed mt-[4vw] mb-[2vw]">Réservation</h2>
+        <h2
+          class="font-title text-titleBase md:text-titleMed mt-[4vw] mb-[2vw]"
+        >
+          Réservation
+        </h2>
 
         <form v-if="roomSelectionData" class="mb-[2vw] tracking-wider">
-
-          <RoomPresentation class="mb-16"/>
+          <RoomPresentation class="mb-16" />
 
           <div class="mx-2 md:mx-4 lg:mx-8">
-
             <!--  user roomSelection      -->
-            <div class="mb-16" >
+            <div class="mb-16">
               <h3 class="font-title text-titleBase mb-4">Votre sélection</h3>
-              <p>Nombre de personne(s) : {{roomSelectionData.val.nOfPers}}</p>
-              <p>Arrivée : {{roomSelectionData.val.startDate}}</p>
-              <p>Départ : {{roomSelectionData.val.endDate}}</p>
-              <p>Prix de base : {{roomSelectionData.price}} €</p>
-
+              <p>Nombre de personne(s) : {{ roomSelectionData.val.nOfPers }}</p>
+              <p>Arrivée : {{ roomSelectionData.val.startDate }}</p>
+              <p>Départ : {{ roomSelectionData.val.endDate }}</p>
+              <p>Prix de base : {{ roomSelectionData.price }} €</p>
             </div>
 
-
             <!--  optional services      -->
-            <div class="mb-16" >
-              <h3 class="font-title text-titleBase mb-4">Sélectionnez des options</h3>
+            <div class="mb-16">
+              <h3 class="font-title text-titleBase mb-4">
+                Sélectionnez des options
+              </h3>
               <ul>
                 <li v-for="option in availableOptions" v-bind:key="option.id">
                   <label class="flex flex-row mb-2">
                     <input
-                        type="checkbox"
-                        :value="option.id"
-                        class="mr-4"
-                        v-model="selectedOptions"
-                        @input="test"
+                      type="checkbox"
+                      :value="option.id"
+                      class="mr-4"
+                      v-model="selectedOptions"
+                      @input="test"
                     />
-                    <span>{{option.name[langStore.lang]}}</span>
-                    <span class="flex-1 border-b border-b-primary h-0 mt-auto mb-1 mx-1"></span>
-                    <span class="">{{option.u_price}} €</span>
-
+                    <span>{{ option.name[langStore.lang] }}</span>
+                    <span
+                      class="flex-1 border-b border-b-primary h-0 mt-auto mb-1 mx-1"
+                    ></span>
+                    <span class="">{{ option.u_price }} €</span>
                   </label>
                 </li>
               </ul>
             </div>
 
-
             <!--  bank card      -->
-            <div class="mb-16" >
+            <div class="mb-16">
               <h3 class="font-title text-titleBase mb-4">Paiement</h3>
               <p>Numéro de carte bleu :</p>
               <input
-                  type="text"
-                  class="mr-4"
-                  v-model="cb"
-                  @input="test($event.target.value)"
+                type="text"
+                class="mr-4"
+                v-model="cb"
+                @input="test($event.target.value)"
               />
             </div>
 
             <!--  total        -->
-            <div class="mb-16" >
+            <div class="mb-16">
               <h3 class="font-title text-titleBase mb-4">Prix du séjour</h3>
-              <p>
-                {{totalPrice + selectedOptionsPrice}} €
-              </p>
+              <p>{{ totalPrice + selectedOptionsPrice }} €</p>
             </div>
           </div>
 
-
-
-
           <div class="flex flex-col items-center gap-4">
-            <button type="button" v-on:click="checkout"
-                    class="bg-accent flex justify-center items-center w-64 h-9 text-xl tracking-wider text-tertiary font-semibold font-content uppercase"
-            >Réserver</button>
+            <button
+              type="button"
+              v-on:click="checkout"
+              class="bg-accent flex justify-center items-center w-64 h-9 text-xl tracking-wider text-tertiary font-semibold font-content uppercase"
+            >
+              Réserver
+            </button>
 
-            <button type="button" v-on:click="cancel"
-                    class="bg-red-200 flex justify-center items-center w-64 h-9 text-xl tracking-wider text-tertiary font-semibold font-content uppercase"
-            >Annuler</button>
+            <button
+              type="button"
+              v-on:click="cancel"
+              class="bg-red-200 flex justify-center items-center w-64 h-9 text-xl tracking-wider text-tertiary font-semibold font-content uppercase"
+            >
+              Annuler
+            </button>
           </div>
         </form>
-
       </div>
-
-
     </div>
-
   </main>
-  <FooterSection v-if="sections.length" :title="sections[7].title" :uri="sections[7].uri" />
+  <FooterSection
+    v-if="sections.length"
+    :title="sections[7].title"
+    :uri="sections[7].uri"
+  />
 </template>
 
-
 <script setup>
-
 import HeaderSection from "../components/sections/HeaderSection.vue";
 import RoomPresentation from "../components/blocks/RoomPresentation.vue";
 import FooterSection from "../components/sections/FooterSection.vue";
 import router from "../router";
-import {computed, ref, toRaw} from "vue";
+import { computed, ref, toRaw } from "vue";
 import { i18n } from "../utils/i18n";
 import { useLangStore } from "../stores/lang";
-import {error, log, warn} from "../utils/console";
+import { error, log, warn } from "../utils/console";
 import axios from "axios";
-import {useRoomSelectionStore} from "../stores/roomSelection";
-import {useOptionsStore} from "../stores/options";
+import { useRoomSelectionStore } from "../stores/roomSelection";
+import { useOptionsStore } from "../stores/options";
 
 // check if booking can be performed : need data in roomStore
-const roomSelection = useRoomSelectionStore()
+const roomSelection = useRoomSelectionStore();
 try {
   if (!toRaw(roomSelection).startDate || !toRaw(roomSelection).endDate) {
-    router.back()
+    router.back();
   }
 } catch (e) {
-  error(e)
-  router.push('/')
+  error(e);
+  router.push("/");
 }
 
 const props = defineProps({
-  sections: Array
-})
+  sections: Array,
+});
 
-const langStore = useLangStore()
-const dico = i18n
+const langStore = useLangStore();
+const dico = i18n;
 
 const optionStore = useOptionsStore();
-const availableOptions = ref()
-const selectedOptions = ref([])
-const totalPrice= ref(roomSelection.price)
+const availableOptions = ref();
+const selectedOptions = ref([]);
+const totalPrice = ref(roomSelection.price);
 
-const roomSelectionData = ref()
+const roomSelectionData = ref();
 
 axios
-    .get(`${import.meta.env.VITE_API_BASE_URL}/optional-services`)
-    .then((resp) => {
-      log('AXIOS !!!!!!!')
-      //log(resp.data)
-      availableOptions.value = toRaw(resp.data.map(i => i))
-      roomSelectionData.value = toRaw(roomSelection)
-    })
-    .catch(err => {
-      error(err)
-    })
-
+  .get(`${import.meta.env.VITE_API_BASE_URL}/optional-services`)
+  .then((resp) => {
+    log("AXIOS !!!!!!!");
+    //log(resp.data)
+    availableOptions.value = toRaw(resp.data.map((i) => i));
+    roomSelectionData.value = toRaw(roomSelection);
+  })
+  .catch((err) => {
+    error(err);
+  });
 
 const selectedOptionsPrice = computed(() => {
   if (availableOptions.value) {
-    let listOfOptions = availableOptions.value.filter(opt => selectedOptions.value.includes(opt.id)).map(e => toRaw(e))
-    console.log(listOfOptions)
+    let listOfOptions = availableOptions.value
+      .filter((opt) => selectedOptions.value.includes(opt.id))
+      .map((e) => toRaw(e));
+    console.log(listOfOptions);
     return listOfOptions.reduce((prev, cur) => {
       // apply pricing rules
-      return prev + cur.u_price * (cur.by_person ? roomSelection.val.nOfPers : 1) * (cur.nb_day>0 ? Math.ceil(roomSelection.val.nOfNights/cur.nb_day) : 1)
-    },0)
+      return (
+        prev +
+        cur.u_price *
+          (cur.by_person ? roomSelection.val.nOfPers : 1) *
+          (cur.nb_day > 0
+            ? Math.ceil(roomSelection.val.nOfNights / cur.nb_day)
+            : 1)
+      );
+    }, 0);
   }
-  return 0
-})
-
+  return 0;
+});
 
 function cancel() {
-  const eraseRoomSelection = new Promise((resolve, reject)=> {
-    resolve(
-        roomSelection.erase()
-    )
-  })
+  const eraseRoomSelection = new Promise((resolve, reject) => {
+    resolve(roomSelection.erase());
+  });
 
   eraseRoomSelection
-      .then(()=> {
-        router.push("/selection")
-      })
-      .catch(e => error(e))
+    .then(() => {
+      router.push("/selection");
+    })
+    .catch((e) => error(e));
 }
 function checkout() {
-
   /*
-  * UPDATE BOOKING : PENDING => VALIDATED
-  * */
+   * UPDATE BOOKING : PENDING => VALIDATED
+   * */
 
   //router.push("/checkout")
   const payload = {
     begin_date: roomSelection.val.startDate,
     end_date: roomSelection.val.endDate,
     rooms_id: roomSelection.val.roomId,
-    customers_id: 1
-  }
+    customers_id: 1,
+  };
 
   axios
-      .update(`${import.meta.env.VITE_API_BASE_URL}/booking`, payload, {
-        headers: {
-          'Authorization': `Bearer ${window.sessionStorage.getItem('token')}`
-        }
-      })
-      .then((resp)=>{
-        console.log(resp)
-      })
-      .catch(e => error(e))
-
+    .put(`${import.meta.env.VITE_API_BASE_URL}/booking`, payload, {
+      headers: {
+        Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+      },
+    })
+    .then((resp) => {
+      console.log(resp);
+    })
+    .catch((e) => error(e));
 }
 
 function test(num) {
-  log(num)
+  log(num);
 }
-
 </script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,51 +1,53 @@
-<template >
+<template>
   <HeaderSection />
   <main class="h-full w-full overflow-hidden">
-    <LoginForm @connectionStatus="(data) => {sayHello(data)}"/>
+    <LoginForm
+      @connectionStatus="
+        (data) => {
+          sayHello(data);
+        }
+      "
+    />
   </main>
-  <FooterSection v-if="sections.length"  :title="sections[7].title" :uri="sections[7].uri" />
+  <FooterSection
+    v-if="sections.length"
+    :title="sections[7].title"
+    :uri="sections[7].uri"
+  />
 </template>
 
 <script>
 export default {
-  name: "LoginView"
-}
+  name: "LoginView",
+};
 </script>
 
 <script setup>
-
 import HeaderSection from "../components/sections/HeaderSection.vue";
 import FooterSection from "../components/sections/FooterSection.vue";
 import LoginForm from "../components/forms/LoginForm.vue";
 import router from "../router";
 
 const props = defineProps({
-  sections: Array
-})
+  sections: Array,
+});
 
-const emit = defineEmits(
-    {'login': {},
-      'isAdmin':{}
-    }
-)
+const emit = defineEmits({ login: {}, isAdmin: {} });
 
 function sayHello(data) {
-  console.log("vous êtes connecté")
+  console.log("vous êtes connecté");
   //console.group(data)
 
   if (data.token) {
-    emit('login')
+    emit("login");
   }
 
   if (data.role.toString() === "admin") {
-    emit('isAdmin')
-    console.log("redirect to admin panel")
-    router.push("/admin")
+    emit("isAdmin");
+    console.log("redirect to admin panel");
+    router.push("/admin");
   } else {
-    router.push("/")
+    router.back();
   }
 }
-
-
-
 </script>
