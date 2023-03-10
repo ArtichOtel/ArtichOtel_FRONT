f1befca211e549cf5eafcd3365aaaa3fd916b2dc
butch <au.bouchard@gmail.com>
2023-03-10 14:59:51 +0100

    fix modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,10 +1,10 @@
 <template>
   <div
     v-show="modalActive"
-    class="absolute backdrop-blur-md fixed top-0 left-0 z-90 w-screen p-[2.6vw]
-    flex flex-col justify-center items-center py-8 md:py-16 "
+    class="absolute backdrop-blur-md fixed top-0 left-0 z-90 w-screen h-full xpx-[2.6vw]
+    flebx flex-col justify-center items-center py-8 md:py-16 "
   >
-    <div class="max-w-[1024px]">
+    <div class="max-w-[1024px] mx-auto">
       <div class="bg-primary text-secondary text-titleBase w-full flex p-[2.6vw] flex-row justify-between items-start">
         <h2 class="font-title text-center">{{title}}</h2>
 
